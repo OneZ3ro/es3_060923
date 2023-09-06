3edd129993d7980eb52eaf59b1c17b523da2e574
Angelo Moreno <emilioangelo665@gmail.com>
2023-09-06 16:45:45 +0200

    es 4 done

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,19 +1,26 @@
+import { Component } from "react";
 import { Button, Card, Col } from "react-bootstrap";
 
-const SingleBook = (props) => {
-  return (
-    <Col>
-      <Card>
-        <Card.Img variant="top" src={props.myObj.img} />
-        <Card.Body>
-          <Card.Title style={{ maxHeight: "48px", overflow: "hidden" }}>
-            {props.myObj.title}
-          </Card.Title>
-          <Card.Text>Questo libro costa: {props.myObj.price}</Card.Text>
-          <Button variant="primary">Compra</Button>
-        </Card.Body>
-      </Card>
-    </Col>
-  );
-};
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
+
+  render() {
+    return (
+      <Col>
+        <Card>
+          <Card.Img variant="top" src={this.props.myObj.img} />
+          <Card.Body>
+            <Card.Title style={{ maxHeight: "48px", overflow: "hidden" }}>
+              {this.props.myObj.title}
+            </Card.Title>
+            <Card.Text>Questo libro costa: {this.props.myObj.price}</Card.Text>
+            <Button variant="primary">Compra</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  }
+}
 export default SingleBook;
