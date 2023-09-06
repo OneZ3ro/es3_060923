30749ae4ee64b43686b2720a62b2d9757fce104f
Angelo Moreno <emilioangelo665@gmail.com>
2023-09-06 16:59:53 +0200

    es 5 half done

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -8,17 +8,38 @@ class SingleBook extends Component {
 
   render() {
     return (
-      <Col>
-        <Card>
-          <Card.Img variant="top" src={this.props.myObj.img} />
-          <Card.Body>
-            <Card.Title style={{ maxHeight: "48px", overflow: "hidden" }}>
-              {this.props.myObj.title}
-            </Card.Title>
-            <Card.Text>Questo libro costa: {this.props.myObj.price}</Card.Text>
-            <Button variant="primary">Compra</Button>
-          </Card.Body>
-        </Card>
+      <Col
+        onClick={() => {
+          this.setState({ selected: true });
+        }}
+      >
+        {this.state.selected ? (
+          <Card style={{ border: "3px solid red" }}>
+            <Card.Img variant="top" src={this.props.myObj.img} />
+            <Card.Body>
+              <Card.Title style={{ maxHeight: "48px", overflow: "hidden" }}>
+                {this.props.myObj.title}
+              </Card.Title>
+              <Card.Text>
+                Questo libro costa: {this.props.myObj.price}
+              </Card.Text>
+              <Button variant="primary">Compra</Button>
+            </Card.Body>
+          </Card>
+        ) : (
+          <Card>
+            <Card.Img variant="top" src={this.props.myObj.img} />
+            <Card.Body>
+              <Card.Title style={{ maxHeight: "48px", overflow: "hidden" }}>
+                {this.props.myObj.title}
+              </Card.Title>
+              <Card.Text>
+                Questo libro costa: {this.props.myObj.price}
+              </Card.Text>
+              <Button variant="primary">Compra</Button>
+            </Card.Body>
+          </Card>
+        )}
       </Col>
     );
   }
